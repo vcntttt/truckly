9234123a30df2a492a5c8578e9b38b04e1c1347a
Vicente Rivera <vrivera.dev@gmail.com>
2025-06-01 23:44:17 -0400

    feat: shadcn setup

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
@@ -10,4 +11,9 @@ export default defineConfig({
     tailwindcss(),
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
