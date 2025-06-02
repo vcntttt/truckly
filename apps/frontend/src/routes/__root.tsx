4743978228603e0e1171f1b130d13106f8af6f20
Vicente Rivera <vrivera.dev@gmail.com>
2025-06-01 23:52:02 -0400

    feat: head minimo con titulo y descripcion

--- apps/frontend/src/routes/__root.tsx
+++ apps/frontend/src/routes/__root.tsx
@@ -1,11 +1,23 @@
 import { Providers } from "@/components/providers";
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, HeadContent, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        title: "Truckly",
+      },
+      {
+        name: "description",
+        content: "Plataforma de gestión de flotas de vehículos",
+      },
+    ],
+  }),
   component: () => (
     <>
       <Providers>
+        <HeadContent />
         <Outlet />
         <TanStackRouterDevtools />
       </Providers>
