404fa17492909c5a977bd616912986275331e12a
Vicente Rivera <vrivera.dev@gmail.com>
2025-06-02 21:18:46 -0400

    feat(dashboard): sidebar header con branding

--- apps/frontend/src/components/dashboard/navigation/app-sidebar.tsx
+++ apps/frontend/src/components/dashboard/navigation/app-sidebar.tsx
@@ -1,44 +1,38 @@
 import {
   Sidebar,
   SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import { Link, useLocation } from "@tanstack/react-router";
 import { sections } from "./sections";
+import { SidebarMenuHeader } from "./sidebar-header";
 
 export function AppSidebar() {
   const location = useLocation();
 
   return (
     <Sidebar collapsible="icon">
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Hola</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {sections.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <Link
-                      to={item.url}
-                      className={
-                        location.pathname === item.url ? "font-semibold" : ""
-                      }
-                    >
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+      <SidebarMenuHeader />
+      <SidebarContent className="mt-12">
+        <SidebarMenu>
+          {sections.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton asChild>
+                <Link
+                  to={item.url}
+                  className={
+                    location.pathname === item.url ? "font-semibold" : ""
+                  }
+                >
+                  <item.icon />
+                  <span>{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
       </SidebarContent>
     </Sidebar>
   );
