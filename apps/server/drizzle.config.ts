1e34beb9614ff009fe541bc33c225074f0109a7f
Valdess <fvaldes2023@alu.uct.cl>
2025-06-06 19:04:32 -0400

    fix():drizzle.config.ts
    arregle el archivo cambiandolo de manera quitando el satiesfies config

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -1,5 +1,3 @@
-import type { Config } from "drizzle-kit";
-
 export default {
     schema: "./src/db/schema.ts",
     out: "./drizzle/migrations",
@@ -7,4 +5,4 @@ export default {
     dbCredentials: {
         connectionString: process.env.DATABASE_URL!,
     },
-} satisfies Config;
+};
