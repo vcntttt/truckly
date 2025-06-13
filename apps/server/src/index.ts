9a503debf1591ffa6c032270b7a67b2d79796f23
Juanfrax2 <juan.munoz.veloso@gmail.com>
2025-06-13 18:17:50 -0400

    feat: configuración para despliegue en Vercel

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,7 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/', (c) => c.text('Hello Hono desde Vercel'));
 
-export default app
+export default app;
