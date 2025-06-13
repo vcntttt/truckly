ac46b35565654c108cec4bad745637b4e9a33348
Vicente Rivera <vrivera.dev@gmail.com>
2025-06-13 16:00:26 -0400

    refactor(dashboard): data de ejemplo separada

--- apps/frontend/src/routes/dashboard/vehiculos.tsx
+++ apps/frontend/src/routes/dashboard/vehiculos.tsx
@@ -1,10 +1,8 @@
 import { DataTable } from "@/components/dashboard/tables/data-table";
 import { VehiclesActions } from "@/components/dashboard/tables/vehicles/vehicles-actions";
-import {
-  vehiclesColumns,
-  type Vehiculo,
-} from "@/components/dashboard/tables/vehicles/vehicles-columns";
+import { vehiclesColumns } from "@/components/dashboard/tables/vehicles/vehicles-columns";
 import { DataTableViewOptions } from "@/components/dashboard/tables/view-options";
+import { vehiculos } from "@/lib/data";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/dashboard/vehiculos")({
@@ -12,57 +10,6 @@ export const Route = createFileRoute("/dashboard/vehiculos")({
 });
 
 function RouteComponent() {
-  const vehiculos: Vehiculo[] = [
-    {
-      id: 1,
-      patente: "JHRX-12",
-      marca: "Mercedes-Benz",
-      modelo: "Sprinter",
-      year: 2021,
-      tipo: "van",
-    },
-    {
-      id: 2,
-      patente: "KLPT-34",
-      marca: "Toyota",
-      modelo: "Hilux",
-      year: 2018,
-      tipo: "pick-up",
-    },
-    {
-      id: 3,
-      patente: "MNBV-56",
-      marca: "Volvo",
-      modelo: "FH",
-      year: 2020,
-      tipo: "camión",
-    },
-    {
-      id: 4,
-      patente: "ZXCV-78",
-      marca: "Isuzu",
-      modelo: "NQR",
-      year: 2019,
-      tipo: "camión",
-    },
-    {
-      id: 5,
-      patente: "QWER-90",
-      marca: "Ford",
-      modelo: "Transit",
-      year: 2022,
-      tipo: "van",
-    },
-    {
-      id: 6,
-      patente: "TYUI-11",
-      marca: "Nissan",
-      modelo: "NV350",
-      year: 2023,
-      tipo: "furgón",
-    },
-  ];
-
   return (
     <DataTable
       columns={vehiclesColumns}
