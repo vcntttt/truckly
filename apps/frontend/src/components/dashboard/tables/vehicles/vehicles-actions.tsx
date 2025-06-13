a954f0f347116fbbf3377797483975e5c3c09759
Vicente Rivera <vrivera.dev@gmail.com>
2025-06-13 00:06:39 -0400

    feat(dashboard/vehiculos): Base para las acciones

--- apps/frontend/src/components/dashboard/tables/vehicles/vehicles-actions.tsx
+++ apps/frontend/src/components/dashboard/tables/vehicles/vehicles-actions.tsx
@@ -1,12 +1,43 @@
+import { useState } from "react";
+import { Plus, SlidersHorizontal } from "lucide-react";
 import type { Table } from "@tanstack/react-table";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import type { Vehiculo } from "./vehicles-columns";
 
 export const VehiclesActions = ({ table }: { table: Table<Vehiculo> }) => {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
   return (
     <div className="flex gap-4">
-      <Button variant={"outline"}>Crear</Button>
-      <Button variant={"outline"}>Filtrar</Button>
+      <Dialog open={isFormOpen} onOpenChange={setIsFormOpen}>
+        <DialogTrigger asChild>
+          <Button variant="outline" className="flex items-center gap-2">
+            <Plus className="h-4 w-4" />
+            Registrar nuevo
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Registrar Nuevo Vehiculo</DialogTitle>
+            <DialogDescription>
+              Completa el formulario para agregar un nuevo vehículo.
+            </DialogDescription>
+          </DialogHeader>
+          <p>form</p>
+        </DialogContent>
+      </Dialog>
+      <Button variant={"outline"}>
+        <SlidersHorizontal />
+        Filtrar
+      </Button>
     </div>
   );
 };
