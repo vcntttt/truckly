f644e5f398e4c1d19b056b681e7ee72a0fd572b2
Valdess <fvaldes2023@alu.uct.cl>
2025-06-14 17:52:13 -0400

    feat(endpoint):endpoint de administrador de asignaciones
    estan todas creadas ya testeadas en bruno y todos dieron bien

--- apps/server/src/trpc/root.ts
+++ apps/server/src/trpc/root.ts
@@ -3,7 +3,7 @@ import { protectedProcedure, adminProcedure } from './procedures';
 import { vehiculoRouter } from '../routes/vehiculos';
 import { asignacionRouter } from '../routes/asignaciones';
 import { vehiculosAdminRouter } from '../routes/vehiculosadmin';
-
+import { asignacionesAdminRouter } from '../routes/asignacionesadmin';
 
 export const appRouter = router({
   perfil: protectedProcedure.query(({ ctx }) => ctx.user),
@@ -11,6 +11,7 @@ export const appRouter = router({
   vehiculos: vehiculoRouter,
   asignaciones: asignacionRouter,
   vehiculosadmin: vehiculosAdminRouter,
+  asignacionesadmin: asignacionesAdminRouter,
 });
 
 export type AppRouter = typeof appRouter;
