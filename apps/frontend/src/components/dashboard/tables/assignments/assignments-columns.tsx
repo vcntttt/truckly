98e6bf7146d839510e02d333c8342f75f6ef887a
Vicente Rivera <vrivera.dev@gmail.com>
2025-06-15 18:22:32 -0400

    feat(dashboard/asignaciones): Eliminar asignacion de la db

--- apps/frontend/src/components/dashboard/tables/assignments/assignments-columns.tsx
+++ apps/frontend/src/components/dashboard/tables/assignments/assignments-columns.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import type { ColumnDef } from "@tanstack/react-table";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { SquarePen, Trash2 } from "lucide-react";
+import { Loader2, SquarePen, Trash2 } from "lucide-react";
 import { EditAssignmentForm } from "@/components/dashboard/forms/edit-assignment";
 import {
   Sheet,
@@ -11,6 +12,19 @@ import {
   SheetDescription,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useTRPC } from "@/lib/trpc";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 import type { Asignaciones } from "@/types";
 
 export const assignmentsColumns: ColumnDef<Asignaciones>[] = [
@@ -85,6 +99,13 @@ export const assignmentsColumns: ColumnDef<Asignaciones>[] = [
     header: "Acciones",
     cell: ({ row }) => {
       const assignment = row.original;
+      const trpc = useTRPC();
+      const deleteAsignacionMutation = useMutation(
+        trpc.asignacionesadmin.delete.mutationOptions()
+      );
+      const getAsignacionesQueryKey = trpc.asignacionesadmin.getAll.queryKey();
+      const queryClient = useQueryClient();
+
       return (
         <div className="flex items-center gap-2">
           <Sheet>
@@ -105,9 +126,49 @@ export const assignmentsColumns: ColumnDef<Asignaciones>[] = [
               </div>
             </SheetContent>
           </Sheet>
-          <Button size={"icon"} variant="outline">
-            <Trash2 />
-          </Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button size={"icon"} variant="outline">
+                <Trash2 />
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Eliminar Asignación</DialogTitle>
+                <DialogDescription>
+                  Esta acción no se puede deshacer.
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="outline">Cancelar</Button>
+                </DialogClose>
+                <Button
+                  variant={"destructive"}
+                  onClick={async () => {
+                    try {
+                      await deleteAsignacionMutation.mutate({
+                        id: assignment.id,
+                      });
+                      queryClient.invalidateQueries({
+                        queryKey: getAsignacionesQueryKey,
+                      });
+                      toast.success("Vehículo eliminado exitosamente");
+                    } catch (error) {
+                      toast.error("Error al eliminar vehículo");
+                      console.error("Error al eliminar vehículo:", error);
+                    }
+                  }}
+                >
+                  {deleteAsignacionMutation.isPending ? (
+                    <Loader2 size={16} className="animate-spin" />
+                  ) : (
+                    <p>Eliminar Asignación</p>
+                  )}
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
       );
     },
