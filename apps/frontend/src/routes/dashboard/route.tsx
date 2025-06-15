59dcdcc9218dcfd75943536af05702e5156f4fc5
Vicente Rivera <vrivera.dev@gmail.com>
2025-06-14 20:09:36 -0400

    feat(frontend): Rutas de admin y de conductor protegidas

--- apps/frontend/src/routes/dashboard/route.tsx
+++ apps/frontend/src/routes/dashboard/route.tsx
@@ -1,4 +1,9 @@
-import { createFileRoute, Outlet, useLocation } from "@tanstack/react-router";
+import {
+  createFileRoute,
+  Outlet,
+  redirect,
+  useLocation,
+} from "@tanstack/react-router";
 import {
   SidebarInset,
   SidebarProvider,
@@ -7,9 +12,22 @@ import {
 import { AppSidebar } from "@/components/dashboard/navigation/app-sidebar";
 import { ToggleThemeButton } from "@/components/toggle-theme";
 import { sections } from "@/components/dashboard/navigation/sections";
+import { useSession } from "@/lib/auth-client";
+import { authClient } from "@/lib/auth-client";
 
 export const Route = createFileRoute("/dashboard")({
   component: DashboardLayout,
+  beforeLoad: async () => {
+    const { data: session } = await authClient.getSession();
+
+    if (!session || session.user.role !== "admin")
+      throw redirect({
+        to: "/",
+        search: {
+          redirect: location.href,
+        },
+      });
+  },
 });
 
 function DashboardLayout() {
@@ -19,24 +37,29 @@ function DashboardLayout() {
     sections.find((section) => section.url === currentPath)?.title ||
     "Dashboard";
 
-  return (
-    <SidebarProvider>
-      <AppSidebar />
+  const { data: session } = useSession();
 
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex w-full items-center justify-between gap-2 px-4">
-            <div className="flex items-center gap-2">
-              <SidebarTrigger className="-ml-1" />
-              <h1 className="font-semibold text-lg">{currentTitle}</h1>
+  if (!session) return null;
+
+  if (session.user.role !== "admin")
+    return (
+      <SidebarProvider>
+        <AppSidebar />
+
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+            <div className="flex w-full items-center justify-between gap-2 px-4">
+              <div className="flex items-center gap-2">
+                <SidebarTrigger className="-ml-1" />
+                <h1 className="font-semibold text-lg">{currentTitle}</h1>
+              </div>
+              <ToggleThemeButton />
             </div>
-            <ToggleThemeButton />
-          </div>
-        </header>
-        <main className="group-has-[[data-collapsible=icon]]/sidebar-wrapper:p-0 p-4 container mx-auto">
-          <Outlet />
-        </main>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+          </header>
+          <main className="group-has-[[data-collapsible=icon]]/sidebar-wrapper:p-0 p-4 container mx-auto">
+            <Outlet />
+          </main>
+        </SidebarInset>
+      </SidebarProvider>
+    );
 }
