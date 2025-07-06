9d46067c2d2fc9f71ae8f4c7968fb813003708fe
Valdess <fvaldes2023@alu.uct.cl>
2025-07-05 23:59:37 -0400

    Update trycatch.ts

--- apps/server/src/trycatch.ts
+++ apps/server/src/trycatch.ts
@@ -17,12 +17,12 @@ export type Result<T, E = Error> = Success<T> | Failure<E>;
  * @returns Un objeto con la forma { data, error }
  */
 export async function tryCatch<T, E = Error>(
-  promise: Promise<T>
+    promise: Promise<T>
 ): Promise<Result<T, E>> {
-  try {
-    const data = await promise;
-    return { data, error: null };
-  } catch (error) {
-    return { data: null, error: error as E };
-  }
+    try {
+        const data = await promise;
+        return { data, error: null };
+    } catch (error) {
+        return { data: null, error: error as E };
+    }
 }
