965e2a51c24807ffac607661685cdb461da8d4cd
Vicente Rivera <vrivera.dev@gmail.com>
2025-07-06 21:49:17 -0400

    fixeito de cookies, entre otras cosas

--- apps/frontend/src/routes/__root.tsx
+++ apps/frontend/src/routes/__root.tsx
@@ -2,6 +2,7 @@ import {
   createRootRouteWithContext,
   HeadContent,
   Outlet,
+  useRouter,
 } from "@tanstack/react-router";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
@@ -11,11 +12,42 @@ import type { User } from "@/types";
 import type { QueryClient } from "@tanstack/react-query";
 import type { useTRPC } from "@/lib/trpc";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useAuthStore } from "@/lib/auth-store";
+import { useEffect } from "react";
 
 interface RouterContext {
   user: User | null;
   queryClient: QueryClient;
   trpc: ReturnType<typeof useTRPC>;
+}
+
+function RootComponent() {
+  const { refresh, user } = useAuthStore();
+  const router = useRouter();
+
+  useEffect(() => {
+    refresh();
+  }, [refresh]);
+
+  useEffect(() => {
+    const unSub = useAuthStore.subscribe((state) => {
+      if (state.user === null && window.location.pathname !== "/") {
+        router.navigate({ to: "/" });
+      }
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [router, user]);
+
+  return (
+    <>
+      <HeadContent />
+      <Outlet />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
+  );
 }
 
 export const Route = createRootRouteWithContext<RouterContext>()({
@@ -36,11 +68,5 @@ export const Route = createRootRouteWithContext<RouterContext>()({
       },
     ],
   }),
-  component: () => (
-    <>
-      <HeadContent />
-      <Outlet />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </>
-  ),
+  component: RootComponent,
 });
